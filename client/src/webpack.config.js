718186c1e42c7b5353edbb82492887b0db824fde
bolatah <bolatah@outlook.com>
2022-01-26 20:41:56 +0100

    Semantic UI & Routes

---
+++ client/src/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  resolve: {
+    alias: {
+      "../../theme.config$": path.join(
+        __dirname,
+        "../my-custom-semantic-theme/theme.config"
+      ),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: "url-loader",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles/[name].[contenthash].css",
+    }),
+  ],
+};
