52890aaac675b5cbeaa6e94a49a46e36d626ed67
bolatah <bolatah@outlook.com>
2022-01-28 23:10:43 +0100

    Like Button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,55 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 
+import { AuthContext } from "../context/auth";
+import LikeButton from "./LikeButton";
+import DeleteButton from "./DeleteButton";
+// import MyPopup from "../util/MyPopup";
+
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
-  function likePost() {
-    console.log("like post!!");
-  }
-  function commentOnPost() {
-    console.log("comment on post!");
-  }
+  const { user } = useContext(AuthContext);
+
   return (
-    <Card>
-      <Card fluid>
-        <Card.Content>
-          <Image
-            floated="right"
-            size="mini"
-            src="https://react.semantic-ui.com/images/avatar/large/molly.png"
-          />
-          <Card.Header>{username}</Card.Header>
-          <Card.Meta as={Link} to={`/post/${id}`}>
-            {moment(createdAt).fromNow(true)}
-          </Card.Meta>
-          <Card.Description>{body}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <p>buttons here</p>
-        </Card.Content>
-        <Button as="div" labelPosition="right" onClick={likePost}>
-          <Button color="teal" basic>
-            <Icon name="heart" />
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        {/*         <MyPopup content="Comment on post">
+          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+            <Button color="blue" basic>
+              <Icon name="comments" />
+            </Button>
+            <Label basic color="blue" pointing="left">
+              {commentCount}
+            </Label>
           </Button>
-          <Label basic color="teal" pointing="left">
-            {likeCount}
-          </Label>
-        </Button>
-        <Button as="div" labelPosition="right" onClick={commentOnPost}>
-          <Button color="blue" basic>
-            <Icon name="comments" />
-          </Button>
-          <Label basic color="blue" pointing="left">
-            {commentCount}
-          </Label>
-        </Button>
-      </Card>
-      <Card></Card>
+        </MyPopup> */}
+        {user && user.username === username && <DeleteButton postId={id} />}
+      </Card.Content>
     </Card>
   );
 }
-
 export default PostCard;
